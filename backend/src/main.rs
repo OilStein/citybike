4a9e9b202315ae04d00cd06cda8d6327a30ed6f3
NikoWSL <nikobranart@gmail.com>
2023-05-10 12:40:59 +0300

    local embedded surrealdb instance

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,21 @@
 use std::time::Instant;
 
 use backend::utils::csv_reader::read_files;
+use surrealdb::{engine::local::RocksDb, Surreal};
 
-fn main() {
+#[tokio::main]
+async fn main() -> surrealdb::Result<()> {
     let start = Instant::now();
+
+    let db = Surreal::new::<RocksDb>("temp.db").await?;
+    db.use_ns("test").use_db("test").await?;
+
     // reads stations
     read_files();
 
     let end = Instant::now();
 
     println!("Duration: {:?}", (end - start));
+
+    Ok(())
 }
