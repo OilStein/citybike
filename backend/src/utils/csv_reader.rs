f8b6ae1aef8a5a058cb6c44a3a269301da547afb
NikoWSL <nikobranart@gmail.com>
2023-06-01 19:54:19 +0300

    data: csv datasets

--- backend/src/utils/csv_reader.rs
+++ backend/src/utils/csv_reader.rs
@@ -27,7 +27,6 @@ async fn read_stations(file_name: &str) -> Result<Vec<Station>, Error> {
         .filter(Station::validate)
         .collect();
 
-
     Ok(stations)
 }
 
@@ -88,7 +87,8 @@ async fn read_journeys(file_name: &str) -> Result<Vec<Journey>, Error> {
     // TODO Consider to send data on 100k row vectors to database rather than in a whole vector.
     // Might use lesser amount RAM when reading 1M rows and then sending whole vector.
     let journeys: Vec<Journey> = csv::ReaderBuilder::new()
-        .from_path(path).expect("panic error")
+        .from_path(path)
+        .expect("panic error")
         .deserialize::<CsvJourney>()
         .filter_map(Result::ok)
         .filter_map(|csv_journey| {
@@ -119,7 +119,7 @@ async fn read_journeys(file_name: &str) -> Result<Vec<Journey>, Error> {
         })
         .collect();
 
-        Ok(journeys)
+    Ok(journeys)
 }
 /// Sends journey data to the database in a transaction block.
 // TODO: Consider to move to different file.
@@ -137,16 +137,16 @@ async fn send_journeys_to_db(db: &Surreal<Db>, journeys: Vec<Journey>) -> Result
 /// Initiliazes database with stations and journey that are read from csv files.
 pub async fn read_files(db: &Surreal<Db>) -> Result<(), Error> {
     send_stations_to_db(db, read_stations("stations.csv").await?).await?;
-   
-   // testdata
-   // send_journeys_to_db(db, read_journeys("journeys.csv").await?).await?;
+
+    // testdata
+    // send_journeys_to_db(db, read_journeys("journeys.csv").await?).await?;
 
     // with all of these imported, duration: 48min
     // specs: wsl2 ubuntu restricted to 2 cores and 4GB RAM
     // TODO: journey page query failed
     send_journeys_to_db(db, read_journeys("2021-05.csv").await?).await?;
-    send_journeys_to_db(db, read_journeys("2021-06.csv").await?).await?;
-    send_journeys_to_db(db, read_journeys("2021-07.csv").await?).await?;
+    // send_journeys_to_db(db, read_journeys("2021-06.csv").await?).await?;
+    // send_journeys_to_db(db, read_journeys("2021-07.csv").await?).await?;
 
     Ok(())
 }
